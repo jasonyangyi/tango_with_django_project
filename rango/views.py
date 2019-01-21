d054f2c9e5d8de64cdb9d5d6de7b40fbd02c951a
YIYANG <2377415y@student.gla.ac.uk>
2019-01-21 19:22:24 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
-
 from django.http import  HttpResponse
+from django.shortcuts import render
 def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
-    return HttpResponse("Rango says hey there partner!  <br/> <a href='/rango/about/'>About</a> " )
 def about(request):
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>  ')
+    context_dict = {'boldmessage': "This tutorial has been put together by <Yang Yi>"}
+    return render(request, 'rango/about.html', context=context_dict)
