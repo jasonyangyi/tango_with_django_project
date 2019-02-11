425b64fc262a24ebcc10f4610d300cac93361ee9
YIYANG <2377415y@student.gla.ac.uk>
2019-02-11 22:38:50 +0000

    the final

--- rango/views.py
+++ rango/views.py
@@ -34,21 +34,14 @@ def index(request):
     request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {'categories': category_list, 'pages': page_list}
-
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
     response = render(request, 'rango/index.html',  context=context_dict)
     return response
 
-def about(request):
 
-    ''''if request.session.test_cookie_worked():
-       print("TEST COOKIE WORKED!")
-       request.session.delete_test_cookie()
-    print(request.method)
-    print(request.user)'''
+def about(request):
     context_dict = { }
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
