f5dced224542cf6bc83d4b7650d81645456782cb
YIYANG <2377415y@student.gla.ac.uk>
2019-02-12 15:57:13 +0000

    test

--- rango/views.py
+++ rango/views.py
@@ -141,17 +141,17 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
-def  visitor_cookie_handler(request):
-    visits = int(request.COOKIES.get('visits', '1'))
 
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
         request.session['last_visit'] = str(datetime.now())
     else:
         request.session['last_visit'] = last_visit_cookie
-
     request.session['visits'] = visits
 
 
